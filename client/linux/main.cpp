4f74fadb19b67f299988c45218a591267a95feb6
tomykaira <tomykaira@gmail.com>
2013-12-05 16:01:03 +0900

    Capture latest image by recreate VideoCapture instance

--- client/linux/main.cpp
+++ client/linux/main.cpp
@@ -1,32 +1,33 @@
 #include "opencv2/opencv.hpp"
 #include <vector>
 #include <boost/asio.hpp>
+#include <boost/timer.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include "mysql_image.hpp"
 
 int main(int, char**)
 {
-    cv::VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened()) {  // check if we succeeded
-        std::cerr << "Failed to open camera" << std::endl;
-        return -1;
-    }
-
     MySQLImage my("localhost", "test");
 
     boost::asio::io_service io;
 
     while(1) {
-        boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
+        boost::timer te;
 
+        cv::VideoCapture cap(0); // open the default camera
+        if(!cap.isOpened()) {  // check if we succeeded
+            std::cerr << "Failed to open camera" << std::endl;
+            return -1;
+        }
         cv::Mat frame;
         std::vector<uchar> buf;
         cap >> frame; // get a new frame from camera
         cv::imencode(".png", frame, buf);
         std::string data(buf.begin(), buf.end());
         my.saveImage(data);
-        std::cout << "Sending" << std::endl;
+        cap.release();
 
+        boost::asio::deadline_timer t(io, boost::posix_time::milliseconds(1000.0 - te.elapsed() * 1000));
         t.wait();
     }
 
