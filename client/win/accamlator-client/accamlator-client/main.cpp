971a8b45f1c51a999f79ced42ae72e4440b0cd16
tomykaira <tomykaira@gmail.com>
2013-12-14 18:55:55 +0900

    Split capture.cpp on win

--- client/win/accamlator-client/accamlator-client/main.cpp
+++ client/win/accamlator-client/accamlator-client/main.cpp
@@ -1,10 +1,5 @@
 #define _WIN32_WINNT 0x501
-#include "opencv2/opencv.hpp"
-#include <vector>
-#include <boost/asio.hpp>
-#include <boost/timer.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include "mysql_image.hpp"
+#include "capture.hpp"
 
 int main(int argv, char** argc)
 {
@@ -17,28 +12,8 @@ int main(int argv, char** argc)
     }
 	try {
 		MySQLImage my(host, source);
-
-    boost::asio::io_service io;
-
-    while(1) {
-        boost::timer te;
-
-        cv::VideoCapture cap(0); // open the default camera
-        if(!cap.isOpened()) {  // check if we succeeded
-            std::cerr << "Failed to open camera" << std::endl;
-            return -1;
-        }
-        cv::Mat frame;
-        std::vector<uchar> buf;
-        cap >> frame; // get a new frame from camera
-        cv::imencode(".png", frame, buf);
-        std::string data(buf.begin(), buf.end());
-        my.saveImage(data);
-        cap.release();
-
-        boost::asio::deadline_timer t(io, boost::posix_time::milliseconds(1000.0 - te.elapsed() * 1000));
-        t.wait();
-    }
+		Capture capture(my);
+		capture.runPeriodicCapture();
 	} catch (std::string e) {
 		std::cerr << "Failed to connect to MySQL: " << e << std::endl;
 		return 1;
