3093e096daf5c243c197305e140eab021cd773ab
tomykaira <tomykaira@gmail.com>
2013-12-14 17:04:02 +0900

    Flip if 3rd argument specifies true

--- client/linux/capture.cpp
+++ client/linux/capture.cpp
@@ -1,7 +1,8 @@
 #include "capture.hpp"
 
-Capture::Capture(MySQLImage &my) :
-  cap_(0), my_(my), io_(), timer_(io_, boost::posix_time::milliseconds(interval_))
+Capture::Capture(MySQLImage &my, bool flip) :
+  cap_(0), my_(my), flip_(flip),
+  io_(), timer_(io_, boost::posix_time::milliseconds(interval_))
 {
   if (!cap_.isOpened()) {
     throw "Failed to open device";
@@ -19,14 +20,19 @@ void Capture::runPeriodicCapture()
 
 void Capture::savePeriodically()
 {
-  cv::Mat frame;
+  cv::Mat frame, flipped;
   cap_.retrieve(frame);
 
   if (frame.empty()) {
     std::cerr << "Frame is empty" << std::endl;
   } else {
+    if (flip_) {
+      cv::flip(frame, flipped, -1);
+    } else {
+      flipped = frame;
+    }
     std::vector<uchar> buf;
-    cv::imencode(".png", frame, buf);
+    cv::imencode(".png", flipped, buf);
     std::string data(buf.begin(), buf.end());
     my_.saveImage(data);
   }
--- client/linux/main.cpp
+++ client/linux/main.cpp
@@ -5,13 +5,19 @@ int main(int argv, char** argc)
 {
     std::string host = "localhost";
     std::string source = "test";
-    if (argv == 3)
+    bool flip = false;
+    if (argv >= 3)
     {
         host = std::string(argc[1]);
         source = std::string(argc[2]);
     }
+    if (argv >= 4)
+    {
+        auto arg = std::string(argc[3]);
+        flip = arg == "1" || arg == "true" || arg == "flip";
+    }
     MySQLImage my(host, source);
-    Capture capture(my);
+    Capture capture(my, flip);
 
     capture.runPeriodicCapture();
     return 0;
